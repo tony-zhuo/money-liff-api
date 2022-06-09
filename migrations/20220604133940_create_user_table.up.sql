af935ecd4266fd31b756ef9f2e4b4d17a8e09379
tonyzhuo <tony860725@gmail.com>
2022-06-09 23:43:38 +0800

    feat: user table migration add line_id hash index

--- migrations/20220604133940_create_user_table.up.sql
+++ migrations/20220604133940_create_user_table.up.sql
@@ -1,9 +1,11 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY ,
-    line_id VARCHAR (40) UNIQUE NOT NULL ,
+    line_id VARCHAR (40) NOT NULL ,
     name VARCHAR (20) NOT NULL ,
     avatar_url TEXT DEFAULT NULL ,
     created_at timestamp without time zone DEFAULT NULL ,
     updated_at timestamp without time zone DEFAULT NULL ,
     deleted_at timestamp without time zone DEFAULT NULL
-)
+);
+
+CREATE INDEX line_id_hash_index ON users USING hash(line_id);
